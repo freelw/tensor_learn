2d79d05787efb971c10e711e2e21972c2ba9c038
李望 <liwang@liwangdeiMac.local>
2017-12-02 13:45:57 +0800

    opt

--- start_tensor_dev.sh
+++ start_tensor_dev.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it -v /Users/liwang/project/tensorflow:/share freelw/tensorflow /bin/bash
+docker run -it -v /Users/liwang/project/tensor_learn:/share freelw/tensorflow /bin/bash
