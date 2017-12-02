4ea7e9252250cb55b973eb123ba3eee9bc8a91e3
wangli(李望) <wangli@tencent.com>
2017-12-02 16:36:40 +0800

    print w b

--- decode/main.cpp
+++ decode/main.cpp
@@ -5,15 +5,36 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-
     ifstream ifs("../param");
-
-
     int w = 0;
     int h = 0;
     ifs >> w >> h;
+    double **W = new (double*)[h];
+    for (int i = 0; i < h; ++ i) {
+        W[i] = new double[w];
+        for (int j = 0; j < w; ++j) {
+            W[i][j] = 0;
+        }
+    }
 
+    double *b = new double[h];
+    for (int i = 0; i < h; ++ i) {
+        b[i] = 0;
+    }
 
+    cout << w << " " << b << endl;
+    for (int i = 0; i < h; ++ i) {
+        for (int j = 0; j < w; ++ j) {
+            ifs >> W[i][j];
+            cout << W[i][j] << " ";
+        }
+        cout << endl;
+    }
 
+    for (int i = 0; i < h; ++ i) {
+        ifs >> b[i];
+        cout << b[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
