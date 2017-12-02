da4684df8207288289ed7cdea31801f8677c518e
wangli(李望) <wangli@tencent.com>
2017-12-02 16:31:55 +0800

    decoder

---
+++ decode/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+
+    ifstream ifs("../param");
+
+
+    int w = 0;
+    int h = 0;
+    ifs >> w >> h;
+
+
+
+    return 0;
+}
